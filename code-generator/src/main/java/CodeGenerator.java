6760206d1a9d85112053692a7ff8ac39a3188eba
chengweiping <1623405873@qq.com>
2020-11-21 13:19:47 +0800

    修改

--- code-generator/src/main/java/CodeGenerator.java
+++ code-generator/src/main/java/CodeGenerator.java
@@ -57,8 +57,13 @@ public class CodeGenerator {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
+       // pc.setModuleName(scanner("模块名"));
         pc.setParent("com.cwp.study");
+        pc.setMapper("dao.user.mapper");
+        pc.setEntity("dao.user.entity");
+        pc.setService("biz.user.service");
+        pc.setServiceImpl("biz.user.service.impl");
+        pc.setController("web.user.controller");
         mpg.setPackageInfo(pc);
 
         // 自定义配置
@@ -81,7 +86,7 @@ public class CodeGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                return projectPath + "/src/main/java/com/cwp/study/dao/user/mapper"
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
